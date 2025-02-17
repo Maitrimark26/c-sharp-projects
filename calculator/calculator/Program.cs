9eb7ecc47b812cc1ca3c14876c1b7a775f050bf6
Maitri Markandeywar <106298947+Maitrimark26@users.noreply.github.com>
2025-02-17 21:56:55 +0530

    Add files via upload

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+
+  
+    
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+         Hashtable h=new Hashtable();
+            h.Add("Roll No", 1);
+            h.Add("Name", "Maitri");
+            h.Add("Age", 21);
+            h.Add("City", "Nagpur");
+            foreach (string s in h.Keys)
+            {
+                Console.WriteLine("Key is "+s+" "+ "Value is " + h[s]);   
+            }
+            Console.WriteLine("Total keys are" + h.Count);
+            h.Remove("Roll No");
+            foreach (string s in h.Keys)
+            {
+                Console.WriteLine("Key is " + s + " " + "Value is " + h[s]);
+            }
+            Console.WriteLine("Total keys are" + h.Count);
+
+
+
+        }
+    }
+}
